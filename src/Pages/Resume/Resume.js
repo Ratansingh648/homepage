62733a7303f4eeffc43a97d7b4d20d400dc23507
Ratansingh648 <ratansingh648@gmail.com>
2021-01-13 03:02:34 +0530

    Changing the PDF

--- src/Pages/Resume/Resume.js
+++ src/Pages/Resume/Resume.js
@@ -1,25 +1,26 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import { Document, Page } from "react-pdf";
-import "./Resume.css";
 import SideBar from "../../Components/SideBar/SideBar.js";
-import coverPic from "../../Asset/ratan-pic-1.png";
-import arrow from "../../Asset/arrow-left.svg";
 import resume from "../../Asset/resume.svg";
+import "./Resume.css";
 
+import cv1 from "../../Asset/Ratan Singh - CV-1.jpg"
+import cv2 from "../../Asset/Ratan Singh - CV-1.jpg"
 
 export default function Resume(){
     return(
     <div className="Resume">
         <SideBar />
         <div className="resume-container">
-            <div className="section-1">
-                <div className="resume-instruction">
+            <div className="download-section">
                 <Link to="/download-resume" className="view-resume-button">
                     <img src={resume} alt="" />
                     <h1>Download PDF</h1>
                 </Link>
-                </div>
+            </div>
+            <div className="resume-document">
+                <img className="resume-page" src={cv1} alt="CV-1"/>
+                <img className="resume-page" src={cv2} alt="CV-2" />
             </div>
         </div>
     </div>
